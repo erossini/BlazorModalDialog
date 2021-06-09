46c57f0654dbf71cf24c87c2bc6556139a94329d
Enrico Rossini <enrico.rossini.uk@live.com>
2021-06-09 18:29:15 +0100

    First commit

---
+++ Enums/ModalDialogType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSC.Blazor.Components.ModalDialog.Enums
+{
+	/// <summary>
+	/// Enum ModalDialogType
+	/// </summary>
+	public enum ModalDialogType
+	{
+		/// <summary>
+		/// The ok
+		/// </summary>
+		Ok,
+		/// <summary>
+		/// The ok or cancel
+		/// </summary>
+		OkCancel,
+		/// <summary>
+		/// The delete or cancel
+		/// </summary>
+		DeleteCancel,
+		/// <summary>
+		/// The yes or no
+		/// </summary>
+		YesNo
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # BlazorModalDialog
 Simple Modal Dialog for Blazor WebAssembly
+
+The full explanation how to use this Blazor component is available on [PureSourceCode](https://www.puresourcecode.com/dotnet/blazor/modal-dialog-component-for-blazor/).
